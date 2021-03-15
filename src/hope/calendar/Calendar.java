fffc2d74f8fd069b00e14b75bb2bde0c72d0029b
shinyhope <shinyhope@sk.com>
2021-03-15 16:19:40 +0900

    get maxdays of month

--- src/hope/calendar/Calendar.java
+++ src/hope/calendar/Calendar.java
@@ -1,12 +1,33 @@
 package hope.calendar;
 
+import java.util.Scanner;
+
+
 public class Calendar {
 
+	private static final int[] MAXDAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	
+	public int getMaxdaysOfMonth(int month) {
+		return MAXDAYS[month-1];
+		
+	}
+	
 	public static void main(String[] args) {
-		System.out.println("hello, calendar");
-
+		System.out.println("calendar");
+		Calendar cal = new Calendar();
 
 		
+		int month;
+		
+		System.out.println("input (month):");
+		Scanner sc = new Scanner(System.in);
+		
+		month = sc.nextInt();
+		
+		System.out.println("month : " + month + ", max days of month : " + cal.getMaxdaysOfMonth(month));
+		
+		
+		sc.close();
 	}
 
 }
